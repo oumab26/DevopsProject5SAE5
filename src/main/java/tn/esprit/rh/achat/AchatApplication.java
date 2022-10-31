65539de5316e4baeadc28353ad3ea56c3989f34a
monabeldi <beldi.mona@esprit.tn>
2022-10-31 12:19:49 +0100

    add Stock test

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -1,13 +1,11 @@
 package tn.esprit.rh.achat;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-
 public class AchatApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/tn/esprit/rh/achat/test/StockServiceImplTest.java
@@ -0,0 +1,69 @@
+package tn.esprit.rh.achat.test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.StockServiceImpl;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockServiceImplTest {
+
+	@Autowired
+	private StockServiceImpl stockService;
+
+	@MockBean
+	private StockRepository stockRepository;
+
+	@Test
+	public void testRetrieveAllStocks() {
+		when(stockRepository.findAll())
+				.thenReturn(Stream.of(new Stock(), new Stock(), new Stock()).collect(Collectors.toList()));
+		assertEquals(3, stockService.retrieveAllStocks().size());
+	}
+
+	@Test
+	public void testAddStock() {
+		Stock stock = new Stock();
+		when(stockRepository.save(stock)).thenReturn(stock);
+		assertEquals(stock, stockService.addStock(stock));
+	}
+
+	@Test
+	public void testDeleteStock() {
+		Stock stock = new Stock();
+		stockService.deleteStock(stock.getIdStock());
+		verify(stockRepository, times(1)).deleteById(stock.getIdStock());
+	}
+
+	@Test
+	public void testUpdateStock() {
+		Stock stock = new Stock();
+		when(stockRepository.save(stock)).thenReturn(stock);
+		assertEquals(stock, stockService.updateStock(stock));
+	}
+
+	@Test
+	public void testRetrieveStock() {
+		Stock stock = new Stock();
+		when(stockRepository.findById(stock.getIdStock())).thenReturn(java.util.Optional.of(stock));
+		assertEquals(stock.getIdStock(), stockService.retrieveStock(stock.getIdStock()).getIdStock());
+	}
+
+
+
+}
