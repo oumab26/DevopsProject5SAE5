00a3751b07a426da891280e7ad62f52a5cd10d6d
Achref jlidi <36365666+achrefjlidi@users.noreply.github.com>
2022-11-03 23:45:20 +0100

    fix test

--- src/main/java/tn/esprit/rh/achat/test/FournisseurServiceImplTest.java
+++ src/main/java/tn/esprit/rh/achat/test/FournisseurServiceImplTest.java
@@ -1,11 +1,7 @@
 package tn.esprit.rh.achat.test;
 
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.context.SpringBootTest;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import org.junit.jupiter.api.extension.ExtendWith;
